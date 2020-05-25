68bff650c557b87db1e3fa83300085cf0bb5116b
Raul Guzman <RGuzman@axxonconsulting.com>
2020-05-24 22:42:10 -0300

    End Of Section 11

--- DatingApp/Controllers/AuthController.cs
+++ DatingApp/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.Data;
 using DatingApp.Dtos;
 using DatingApp.Models;
@@ -18,8 +19,10 @@ namespace DatingApp.Controllers
     {
         private readonly IAuthRepository _repo;
         private readonly IConfiguration _config;
-        public AuthController(IAuthRepository repo, IConfiguration config)
+        private readonly IMapper _mapper;
+        public AuthController(IAuthRepository repo, IConfiguration config, IMapper mapper)
         {
+            _mapper = mapper;
             _config = config;
             _repo = repo;
         }
@@ -50,9 +53,9 @@ namespace DatingApp.Controllers
             //throw new Exception("Computer Says No!");
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
-            if(userFromRepo == null)
+            if (userFromRepo == null)
                 return Unauthorized();
-            
+
             var claims = new[]
             {
                 new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
@@ -74,8 +77,12 @@ namespace DatingApp.Controllers
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
-            return Ok(new {
-                token = tokenHandler.WriteToken(token)
+            var user = _mapper.Map<UserForListDto>(userFromRepo);
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token),
+                user
             });
         }
 
--- DatingApp/Dtos/PhotoForReturnDto.cs
+++ DatingApp/Dtos/PhotoForReturnDto.cs
@@ -1,8 +1,8 @@
 using System;
 
-namespace DatingApp.Models
+namespace DatingApp.Dtos
 {
-    public class Photo
+    public class PhotoForReturnDto
     {
         public int Id { get; set; }
 
@@ -15,9 +15,5 @@ namespace DatingApp.Models
         public bool IsMain { get; set; }
 
         public string PublicId { get; set; }
-
-        public User User { get; set; }
-
-        public int UserId { get; set; }
     }
 }
